ecc5e3be987a44c9cb1eb805fef88d6e66153e19
Emanuele-Senis <97055298+Emanuele-Senis@users.noreply.github.com>
2023-05-10 10:17:53 +0200

    Aggiungere i file di progetto.

---
+++ PasswordValidation_Lib/PasswordValidator.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PasswordValidation_Lib
+{
+    /// <summary>
+    /// Autore: Emanuele Alessandro Senis
+    /// ITIS E. Majorana di Grugliasco
+    /// Data: 6 febbraio 2023
+    /// Progetto: validazione password su c# + GUI per mostrarne l'utilizzo
+    /// </summary>
+    public class PasswordValidator
+    {
+        private readonly string _password;
+        public event Action OnPasswordValidated;
+        public event Action OnPasswordInvalid;
+
+        protected int _minLength, _maxLength;
+
+        public List<string> WarningsList { get; protected set; }
+        public PasswordValidator(string password = "", int minLength = 8, int maxLength = 14)
+        {
+            _password = password;
+            WarningsList = new List<string>();
+            _minLength = minLength;
+            _maxLength = maxLength;
+        }
+        public void Validate()
+        {
+            bool isValid = !string.IsNullOrWhiteSpace(_password) &&
+                    IsLengthValid() &&
+                    ContainsAnyUpperCaseChar() &&
+                    ContainsAnyLowerCaseChar() &&
+                    !ContainsWhiteSpaces() &&
+                    ContainsAnySpecialChar();
+            if (isValid)
+            {
+                OnPasswordValidated?.Invoke();
+            }
+            else
+            {
+                OnPasswordInvalid?.Invoke();
+            }
+        }
+        public bool IsLengthValid()
+        {
+            bool isLengthValid = _password.Length >= _minLength && _password.Length <= _maxLength;
+            if (isLengthValid == false)
+            {
+                WarningsList.Add($"the length of the password must be between {_minLength} and {_maxLength} characters");
+            }
+            return isLengthValid;
+        }
+
+        public bool ContainsAnyUpperCaseChar()
+        {
+            bool containsAnyUpperCaseChar = _password.Any(char.IsUpper);
+            if (containsAnyUpperCaseChar == false)
+            {
+                WarningsList.Add("the password must contain at least one uppercase character");
+            }
+            return containsAnyUpperCaseChar;
+        }
+
+        public bool ContainsAnyLowerCaseChar()
+        {
+            bool containsAnyLowerCaseChar = _password.Any(char.IsLower);
+            if (containsAnyLowerCaseChar == false)
+            {
+                WarningsList.Add("the password must contain at least one lowercase character");
+            }
+            return containsAnyLowerCaseChar;
+        }
+
+        public bool ContainsWhiteSpaces()
+        {
+            bool containsWhiteSpaces = _password.Contains(' ');
+            if (containsWhiteSpaces)
+            {
+                WarningsList.Add("the password mustn't contain any white spaces");
+            }
+            return containsWhiteSpaces;
+        }
+
+        public bool ContainsAnySpecialChar()
+        {
+            char[] chars = _password.ToCharArray();
+            foreach (char c in chars)
+            {
+                if ((c >= 32 && c <= 47) || (c >= 58 && c <= 64) || (c >= 91 && c <= 96) || (c >= 123 && c <= 126))
+                {
+                    return true;
+                }
+            }
+            WarningsList.Add("password must contain at least one special character");
+            return false;
+        }
+    }
+}
---
+++ PasswordValidation_WPF_GUI/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PasswordValidation_Lib;
+
+namespace PasswordValidation_WPF_GUI
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private PasswordValidator _validator;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Txt_password_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            _validator = new PasswordValidator(txt_password.Text);
+            _validator.OnPasswordValidated += Validator_OnPasswordValidated;
+            _validator.OnPasswordInvalid += Validator_OnPasswordInvalid;
+            _validator.Validate();
+        }
+
+        private void Validator_OnPasswordInvalid()
+        {
+            lstBx_warnings.Items.Clear();
+            lstBx_warnings.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F24333")); // red colour
+            foreach (string warning in _validator.WarningsList)
+            {
+                lstBx_warnings.Items.Add(warning);
+            }
+        }
+
+        private void Validator_OnPasswordValidated()
+        {
+            lstBx_warnings.Items.Clear();
+            lstBx_warnings.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#4DAA57")); // green colour
+            lstBx_warnings.Items.Add("the password is valid");
+        }
+    }
+}
